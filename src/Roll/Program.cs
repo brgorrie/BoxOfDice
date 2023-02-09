b5ff0f36f19a54352298dbae5af4a2be96f8b75e
Brian Gorrie <brgorrie@gmail.com>
2023-02-09 22:25:36 +0800

    Changes to make a unit test for invalid parameters to main green.

--- src/Roll/Program.cs
+++ src/Roll/Program.cs
@@ -3,6 +3,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("No parameters were provided, please specify a dice roll ie 1d6 or 1D6 or put -? for help.");
+        if (args.Length > 0)
+        {
+            Console.WriteLine("Invalid parameters were provided, please specify a dice roll ie 1d6 or 1D6 or put -? for help.");
+        }
+        else
+        {
+            Console.WriteLine("No parameters were provided, please specify a dice roll ie 1d6 or 1D6 or put -? for help.");
+        }
     }
 }
