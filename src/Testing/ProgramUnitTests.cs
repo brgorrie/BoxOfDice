4ae8095eaeb364043691cbd8375953b439520820
Brian Gorrie <brgorrie@gmail.com>
2023-02-10 00:17:46 +0800

    Added in tests to cover off whitespace and empty strings being passed in.

--- src/Testing/ProgramUnitTests.cs
+++ src/Testing/ProgramUnitTests.cs
@@ -33,6 +33,36 @@ public class ProgramUnitTests
     }
 
     [Fact]
+    public void MainEmptyStringInvalidParametersTest()
+    {
+
+        var output = new StringWriter();
+        Console.SetOut(output);
+
+        Roll.Program.Main(new string[] { "" });
+
+        var expectedValue = "Invalid parameters were provided, please specify a dice roll ie 1d6 or 1D6 or put -? for help.";
+
+        Assert.Contains(expectedValue, output.ToString());
+
+    }
+
+    [Fact]
+    public void MainWhiteSpaceInvalidParametersTest()
+    {
+
+        var output = new StringWriter();
+        Console.SetOut(output);
+
+        Roll.Program.Main(new string[] { "      " });
+
+        var expectedValue = "Invalid parameters were provided, please specify a dice roll ie 1d6 or 1D6 or put -? for help.";
+
+        Assert.Contains(expectedValue, output.ToString());
+
+    }
+
+    [Fact]
     public void MainRoll1D6Test()
     {
 
